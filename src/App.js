25cd1b42a66fe42aa9951210c693d8a4522c2c57
Your Name <you@example.com>
2021-03-16 10:06:45 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { Switch, Route } from "react-router-dom";
+import Login from "./pages/Login"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import register from "./pages/Register"
+
+import Home from "./pages/Home"
+
+
+export default class App extends React.Component{
+  render(){
+    return(
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={register} />
+       
+      </Switch>
+    )
+  }
 }
-
-export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,43 @@
+  
+import React from "react"
+import {Link} from "react-router-dom"
+class Navbar extends React.Component{
+    Logout = () => {
+        localStorage.removeItem("token")
+        localStorage.removeItem("admin")
+        window.location = "/login"
+    }
+    render(){
+        return(
+            <div className="navbar navbar-expand-lg bg-dark navbar-dark">
+                <a className="navbar-brand">
+                    web pengaduan
+                </a>
+
+                {/* show and hide menu */}
+                <button className="navbar-toggler" data-toggle="collapse"
+                data-target="#menu">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                {/* menu */}
+                <div id="menu" className="navbar-collapse collpase">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item">
+                            <Link to="/" className="nav-link">
+                                Home
+                            </Link>
+                        </li>
+                     
+                        <li className="nav-item">
+                            <Link className="nav-link" onClick={() => this.Logout()}>
+                                Logout
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+}
+export default Navbar;
